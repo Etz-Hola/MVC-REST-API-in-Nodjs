d83a349b6e460f9d87fbc8aa54b000f0f49b1f02
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-11 11:49:11 +0100

    opdate

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,16 +1,27 @@
-const data = {};
-data.employees = require('../model/employees.json');
+const data = {
+employees: require('../model/employees.json'),
+setEmployees: function (data) {this.employees = dta;},
+};
 
 const getAllEmployees = (req, res) => {
     res.json(data.employees)
 }
 
 const createNewEmployee = (req, res) => {
-    res.json({
-        "firstname": req.body.firstname, 
-        "lastname": req.body.lastname
-    })
+    const newEmployee = {
+        id: data.employees?.length? data.employees[data.employees.length -1].id + 1 :1,
+            "firstname": req.body.firstname, 
+            "lastname": req.body.lastname
+    }
+
+    if(!newEmploy.firstname || newEmploy.lastname) {
+        return res.status(400).json({'message': 'firstname and lastname required'})
+    }
+
+    data.setEmployees([...data.setEmployees, newEmployee])
+    res.status(201).json(data.employees)
 }
+
 
 const updateEmployee = (req, res) => {
     res.json({
