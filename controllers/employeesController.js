09f030f8d1991ae2ce84410e55ab04ba2c72af04
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-11 14:50:48 +0100

    update

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,51 +1,68 @@
 const data = {
     employees: require('../model/employees.json'),
-    setEmployees: function (data) {this.employees = data;},
-};
+    setEmployees: function (data) { this.employees = data; },
+}
+
 
 const getAllEmployees = (req, res) => {
-    res.json(data.employees);
+    res.json(data.employees)
 }
 
 const createNewEmployee = (req, res) => {
     const newEmployee = {
-        id: data.employees ?.length ? data.employees[data.employees.length -1].id + 1 :1,
-            "firstname": req.body.firstname, 
-            "lastname": req.body.lastname
+        id: data.employees?.length ? data.employees[data.employees.length - 1].id + 1 : 1,
+        "firstname": req.body.firstname,
+        "lastname": req.body.lastname
     }
+    if (!newEmployee.firstname || !newEmployee.lastname) {
+        return res.status(400).json({ 'message': 'firstname and lastname required, must they tell you everything?' })
 
-    if(!newEmployee.firstname || !newEmployee.lastname) {
-        return res.status(400).json({'message': 'firstname and last names are required.'});
     }
 
     data.setEmployees([...data.employees, newEmployee])
     res.status(201).json(data.employees)
 }
 
-
 const updateEmployee = (req, res) => {
-    const employee = data.employees.find(emp => emp.id === parseInt(res.body.id));
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id))
 
-    if(!employee) {
-        return res.status(400).json({message: `Emloyee ID ${req.body.id} not found`})
+    if (!employee) {
+        return res.status(400).json({ 'message': `Employee ID ${req.body.id} not found` })
     }
-    if(req.body.firstname) employee.firstname = req.body.firstname
-    if(req.body.lastname) employee.lastname = req.body.lastname
+    if (req.body.firstname) employee.firstname = req.body.firstname
+    if (req.body.lastname) employee.lastname = req.body.lastname
 
     const filterArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id))
-}
 
+    const unsortedArray = [...filterArray, employee]
+
+    data.setEmployees(unsortedArray.sort((a, b) => a.id > b.id ? 1: a.id < b.id? -1 :1))
+    res.json(data.employees)
+
+}
 
 
 const deleteEmployee = (req, res) => {
-    res.json({"id" : req.body.id})
+    const employee = data.employees.find(emp => emp.id === parseInt(req.body.id));
+    
+    if(!employee) {
+        return res.status(400).json({'message': `Employees ID ${req.body.id} not found `});
+    }
+
+    const filterArray = data.employees.filter(emp => emp.id !== parseInt(req.body.id));
+    data.setEmployees([...filterArray]);
+
+    res.json(data.employees)
 }
 
 const getEmployee = (req, res) => {
-    res.json({"id" : req.params.id})
+    const employee = data.employees.find(emp => emp.id === parseInt(req.params.id));
+
+    if(!employee) {
+        return res.status(400).json({'message': `Employees ID ${req.params.id} not found `});
+    }
+    res.json(employee)
 }
-
-
 
 module.exports = {
     getAllEmployees,
@@ -54,14 +71,4 @@ module.exports = {
     deleteEmployee,
     getEmployee
 }
-
-
-
-
-
-
-
-
-
-
 
